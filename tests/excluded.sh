d183eec4194a3f1cb5b7f92f4bcd69cd94373fae
Hari Sekhon <harisekhon@gmail.com>
2020-06-02 12:35:43 +0100

    added excluded.sh

---
+++ tests/excluded.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2230
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/devops-golang-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# intended only to be sourced by utils.sh
+#
+# split from utils.sh as this is specific to this repo
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+
+isExcluded(){
+    local prog="$1"
+    [[ "$prog" =~ ^\* ]] && return 0
+    # this external git check is expensive, skip it when in CI as using fresh git checkouts
+    is_CI && return 1
+    if type -P git &>/dev/null; then
+        commit="$(git log "$prog" | head -n1 | grep 'commit')"
+        if [ -z "$commit" ]; then
+            return 0
+        fi
+    fi
+    return 1
+}
