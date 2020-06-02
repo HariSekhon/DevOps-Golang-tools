b8bbe8d65cce75ad941798fae917f111c7d0ec71
Hari Sekhon <harisekhon@gmail.com>
2020-06-02 12:35:31 +0100

    added utils.sh

---
+++ tests/utils.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/devops-golang-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# shellcheck disable=SC1090
+. "$srcdir/excluded.sh"
+
+# shellcheck disable=SC1090
+. "$srcdir/../bash-tools/lib/utils.sh"
+
+export COMPOSE_PROJECT_NAME="go-tools"
+
+# shellcheck disable=SC1090
+. "$srcdir/excluded.sh"
