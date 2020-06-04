abd6b22d73bdf20afc11ed2537f135b9e9d9644e
Hari Sekhon <harisekhon@gmail.com>
2020-06-04 18:02:30 +0100

    added all.sh

---
+++ tests/all.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-11-05 23:29:15 +0000 (Thu, 05 Nov 2015)
+#
+#  https://github.com/harisekhon/devops-golang-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/..";
+
+# shellcheck disable=SC1090
+. "bash-tools/lib/utils.sh"
+
+export PROJECT="devops-golang-tools"
+
+section "DevOps Golang Tools ALL Tests"
+
+bash-tools/check_all.sh
+
+#tests/help.sh
+
+bash-tools/run_tests.sh
